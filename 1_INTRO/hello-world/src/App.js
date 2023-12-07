b605b39e3e5ad02a0ecd087538afd1668e046285
jaquelinediasoliveira <jaquelinediasoliveira5@gmail.com>
2023-12-07 16:28:31 -0300

    Primeiro 'Hello World' em React'

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React</h1>
+      <h2>O início</h2>
+    </div>
+  );
+}
+
+export default App;
