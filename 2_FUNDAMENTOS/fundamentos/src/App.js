46675d489f0c1a988ec17898f869a28c0bccf70e
jaquelinediasoliveira <jaquelinediasoliveira5@gmail.com>
2023-12-15 07:16:01 -0300

    Fundamentos React: criando meu primeiro componente

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,14 @@
+import FirstComponent from './components/FirstComponent';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
