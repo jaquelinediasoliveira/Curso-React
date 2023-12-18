818eb813ae8b898ee8b54568003c61c06319929a
jaquelinediasoliveira <jaquelinediasoliveira5@gmail.com>
2023-12-18 14:18:08 -0300

    Adicionando imagens no React e o hook "useState"

---
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+import City from './assets/city.jpg'
+
+import ManageData from './components/ManageData';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando no React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src='/img1.jpg' alt='Paisagem' />
+      </div>
+      {/* Imagem em asset */}
+      <div>
+        <img src={City} alt='Cidade'/>
+      </div>
+      <ManageData/>
+    </div>
+  );
+}
+
+export default App;
